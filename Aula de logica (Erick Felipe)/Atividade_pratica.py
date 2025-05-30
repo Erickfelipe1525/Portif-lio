c34b1780deb178e39b66f839d7b871bc15ef3690
Erick Felipe <erickfelipe5424@gmail.com>
2025-05-30 08:39:36 -0300

    Update Atividade_pratica.py

--- Aula de logica (Erick Felipe)/Atividade_pratica.py
+++ Aula de logica (Erick Felipe)/Atividade_pratica.py
@@ -2,7 +2,10 @@
 numero = input("Digite um número inteiro: ")
 
 # Verificação se é palíndromo
-if numero == numero[::-1]:
-    print("O número é um palíndromo!")
+if numero == numero[::-1]: # fatiamento (slice), [início:fim:passo]
+# :: Não colocamos início nem fim, então pegamos toda a string.
+# -1, ou seja, anda de trás para frente.
+    
+    print("O número {numero} é um palíndromo!")
 else:
-    print("O número não é um palíndromo.")
+    print("O número {numero} não é um palíndromo.")
